838d46909fe0c79686095e5f0a6c1c2517b90afd
xiaobaobaoqing <762377603@qq.com>
2026-09-19 21:29:29 +0800

    FreeRTOS任务调度器实验

--- 7.FreeRTOS_Task7/MDK_ARM/User/FreeRTOSDemo.c
+++ 7.FreeRTOS_Task7/MDK_ARM/User/FreeRTOSDemo.c
@@ -18,10 +18,6 @@ void Task1(void *pvParameters);
 TaskHandle_t Task2Handle = NULL;
 void Task2(void *pvParameters);
 
-List_t TestList;
-ListItem_t TestItem1;
-ListItem_t TestItem2;
-ListItem_t TestItem3;
 
 
 void FreeRTOS_Demo(void)
@@ -57,61 +53,6 @@ void Task1(void *pvParameters)
 
 void Task2(void *pvParameters)
 {
-
-    vListInitialise(&TestList);
-    vListInitialiseItem(&TestItem1);
-    vListInitialiseItem(&TestItem2);
-    vListInitialiseItem(&TestItem3);
-    TestItem1.xItemValue = 40;
-    TestItem2.xItemValue = 60;
-    TestItem3.xItemValue = 50;
-
-    vTaskDelay(10000);
-    Serial_Printf("TestList\t\t0x%p\t\r\n",&TestList);
-    Serial_Printf("TestList->pxIndex\t0x%p\t\r\n",TestList.pxIndex);
-    Serial_Printf("TestList->xListEnd\t0x%p\t\r\n",&(TestList.xListEnd));
-    Serial_Printf("TestItem1\t\t0x%p\t\r\n",&TestItem1);
-    Serial_Printf("TestItem2\t\t0x%p\t\r\n",&TestItem2);
-    Serial_Printf("TestItem3\t\t0x%p\t\r\n",&TestItem3);
-
-    Serial_Printf("插入列表项一\r\n",&TestItem3);
-    vListInsert(&TestList,&TestItem1);
-    Serial_Printf("TestList->xListEnd->pxNext\t0x%p\t\r\n",TestList.xListEnd.pxNext);
-    Serial_Printf("ListItem1->pxNext\t0x%p\t\r\n",TestItem1.pxNext);
-    Serial_Printf("TestList->xListEnd->pxPrevious\t0x%p\t\r\n",TestList.xListEnd.pxNext);
-    Serial_Printf("ListItem1->pxPrevious\t0x%p\t\r\n",TestItem1.pxPrevious);
-
-    Serial_Printf("插入列表项二\r\n",&TestItem2);
-    vListInsert(&TestList,&TestItem2);
-    Serial_Printf("TestList->xListEnd->pxNext\t0x%p\t\r\n",TestList.xListEnd.pxNext);
-    Serial_Printf("ListItem1->pxNext\t0x%p\t\r\n",TestItem1.pxNext);
-    Serial_Printf("ListItem2->pxNext\t0x%p\t\r\n",TestItem2.pxNext);
-    Serial_Printf("TestList->xListEnd->pxPrevious\t0x%p\t\r\n",TestList.xListEnd.pxNext);
-    Serial_Printf("ListItem1->pxPrevious\t0x%p\t\r\n",TestItem1.pxPrevious);
-    Serial_Printf("ListItem2->pxPrevious\t0x%p\t\r\n",TestItem2.pxPrevious);
-
-    Serial_Printf("插入列表项三\r\n",&TestItem3);
-    vListInsert(&TestList,&TestItem3);
-    Serial_Printf("TestList->xListEnd->pxNext\t0x%p\t\r\n",TestList.xListEnd.pxNext);
-    Serial_Printf("ListItem1->pxNext\t0x%p\t\r\n",TestItem1.pxNext);
-    Serial_Printf("ListItem2->pxNext\t0x%p\t\r\n",TestItem2.pxNext);
-    Serial_Printf("ListItem3->pxNext\t0x%p\t\r\n",TestItem3.pxNext);
-    Serial_Printf("TestList->xListEnd->pxPrevious\t0x%p\t\r\n",TestList.xListEnd.pxNext);
-    Serial_Printf("ListItem1->pxPrevious\t0x%p\t\r\n",TestItem1.pxPrevious);
-    Serial_Printf("ListItem2->pxPrevious\t0x%p\t\r\n",TestItem2.pxPrevious);
-    Serial_Printf("ListItem3->pxPrevious\t0x%p\t\r\n",TestItem3.pxPrevious);
-
-    Serial_Printf("删除列表项二\r\n",&TestItem2);
-    vListRemove(&TestItem2);
-    Serial_Printf("TestList->xListEnd->pxNext\t0x%p\t\r\n",TestList.xListEnd.pxNext);
-    Serial_Printf("ListItem1->pxNext\t0x%p\t\r\n",TestItem1.pxNext);
-    Serial_Printf("ListItem3->pxNext\t0x%p\t\r\n",TestItem3.pxNext);
-    Serial_Printf("TestList->xListEnd->pxPrevious\t0x%p\t\r\n",TestList.xListEnd.pxNext);
-    Serial_Printf("ListItem1->pxPrevious\t0x%p\t\r\n",TestItem1.pxPrevious);
-    Serial_Printf("ListItem3->pxPrevious\t0x%p\t\r\n",TestItem3.pxPrevious);
-
-
-
 
 
 
